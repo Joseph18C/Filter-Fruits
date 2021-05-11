35ea8665b5c3826869ee7048b693f8418fb8bccf
Joseph Jc <josephjhair.2306@gmail.com>
2021-05-11 05:47:19 -0500

    Desencadenando los datos de nuestros valores iniciales

---
+++ Logic/FruitsGetInfo.js
@@ -0,0 +1,72 @@
+const FruitsGetInfo = [
+  {
+    id: "Manzanas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Manzana-Roja-390x200.jpg",
+  },
+  {
+    id: "Naranjas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Naranja-Fruta-390x200.jpg",
+  },
+  {
+    id: "Uvas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Uva1-390x200.jpg",
+  },
+  {
+    id: "Mandarinas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Mandarina-390x200.jpg",
+  },
+  {
+    id: "Sandias",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Sandia-390x200.jpg",
+  },
+  {
+    id: "Platanos",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Fruta-Platano-390x200.jpg",
+  },
+  {
+    id: "Fresas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Fresa-390x200.jpg",
+  },
+  {
+    id: "Frambruesa",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Frambuesa-390x200.jpg",
+  },
+  {
+    id: "Higos",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Higo-390x200.jpg",
+  },
+  {
+    id: "Piñas",
+    img: "https://frutass.org/wp-content/uploads/2020/07/Pi%C3%B1a6-390x200.jpg",
+  },
+  {
+    id: "Guanábanas",
+    img: "https://frutass.org/wp-content/uploads/2021/03/Fruta-Guanabana-o-Graviola.jpg",
+  },
+  {
+    id: "Papayas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Papaya-390x200.jpg",
+  },
+  {
+    id: "Melones",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Fruta-Melon-390x200.jpg",
+  },
+  {
+    id: "Maracuyas",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Maracuya-390x200.jpg",
+  },
+  {
+    id: "Guayaba",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Guayaba-390x200.jpg",
+  },
+  {
+    id: "Nueces",
+    img: "https://frutass.org/wp-content/uploads/2020/05/Nuez-390x200.jpg",
+  },
+  {
+    id: "Peras",
+    img: "https://frutass.org/wp-content/uploads/2020/05/pera-380x200.jpg",
+  },
+];
+
+export default FruitsGetInfo;
